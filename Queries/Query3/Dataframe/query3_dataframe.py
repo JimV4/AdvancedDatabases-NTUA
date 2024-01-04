4e60bfd68cff94ed9e11d89b7e7b128b8b390f92
Dimitris Vassiliou <dhmhtrhs.vassiliou@gmail.com>
2024-01-05 00:00:37 +0200

    Fixed small mistakes on query3

--- Queries/Query3/Dataframe/query3_dataframe.py
+++ Queries/Query3/Dataframe/query3_dataframe.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, IntegerType, DoubleType, StringType, DateType
-from pyspark.sql.functions import col, when, regexp_extract, regexp_replace
-import pickle
+from pyspark.sql.functions import col, when, regexp_extract, regexp_replace, year, to_date
 
 spark = SparkSession \
     .builder \
@@ -9,19 +8,17 @@ spark = SparkSession \
     .config("spark.executor.instances", "4") \
     .getOrCreate() \
 
-crimes_df1 = spark.read.csv("Crime_Data_from_2010_to_2019.csv", header=True, inferSchema=True)
-crimes_df2 = spark.read.csv("Crime_Data_from_2020_to_Present.csv", header=True, inferSchema=True)
-crimes_df = crimes_df1.union(crimes_df2)
+crimes_df = spark.read.csv("Crime_Data_from_2010_to_2019.csv", header=True, inferSchema=True)
 
-crimes_df = crimes_df.withColumn("`Date Rptd`", col("Date Rptd").cast(DateType()))
-crimes_df = crimes_df.withColumn("DATE OCC", col("DATE OCC").cast(DateType()))
+crimes_df = crimes_df.withColumn("Date Rptd", to_date(col("Date Rptd"), "MM/dd/yyyy hh:mm:ss a"))
+crimes_df = crimes_df.withColumn("DATE OCC", to_date(col("DATE OCC"), "MM/dd/yyyy hh:mm:ss a"))
 crimes_df = crimes_df.withColumn("Vict Age", col("Vict Age").cast(IntegerType())) 
 crimes_df = crimes_df.withColumn("LAT", col("LAT").cast(DoubleType())) 
 crimes_df = crimes_df.withColumn("LON", col("LON").cast(DoubleType())) 
 
-excluded_null_crimes = crimes_df.filter(crimes_df["Vict Descent"].isNotNull())
+excluded_null_crimes = crimes_df.filter((year(crimes_df["DATE OCC"]) == 2015) & crimes_df["Vict Descent"].isNotNull())
 
-victims_alias = excluded_null_crimes.withColumn("Vict Descent", when(crimes_df["`Vict Descent`"] == "A", "Other Asian")
+victims_alias_crimes_df = excluded_null_crimes.withColumn("Vict Descent", when(crimes_df["`Vict Descent`"] == "A", "Other Asian")
     .when(crimes_df["`Vict Descent`"] == "B", "Black") 
     .when(crimes_df["`Vict Descent`"] == "C", "Chinese")
     .when(crimes_df["`Vict Descent`"] == "D", "Cambodian")
@@ -43,6 +40,7 @@ victims_alias = excluded_null_crimes.withColumn("Vict Descent", when(crimes_df["
 
 income_df = spark.read.csv("LA_income_2015.csv", header=True, inferSchema=True)
 rev_geocoding_df = spark.read.csv("revgecoding.csv", header=True, inferSchema=True)
+rev_geocoding_df = rev_geocoding_df.withColumn("ZIPcode", regexp_extract("ZIPcode", r"(\d+)", 1).cast("int"))
 
 income_df = income_df.withColumn("Estimated Median Income", regexp_replace("Estimated Median Income", '\\$', ''))
 income_df = income_df.withColumn("Estimated Median Income", regexp_replace("Estimated Median Income", ',', '').cast("int"))
@@ -52,15 +50,19 @@ lowest_3_zip_codes = income_df.orderBy(col("Estimated Median Income").asc()).lim
 
 both_zip_codes = highest_3_zip_codes.union(lowest_3_zip_codes)
 
-joined1 = victims_alias.alias("crimes").join(
+joined1 = victims_alias_crimes_df.alias("victims_alias_crimes").join(
     rev_geocoding_df.alias("geocoding"),
-    (col("crimes.LAT") == col("geocoding.LAT")) & (col("crimes.LON") == col("geocoding.LON")),
+    (col("victims_alias_crimes.LAT") == col("geocoding.LAT")) & (col("victims_alias_crimes.LON") == col("geocoding.LON")),
     "inner"
 )
-#.select("geocoding.ZIPcode", "crimes.LAT", "crimes.LON", "crimes.`Vict Descent`")
 
-joined2 = joined1.join(both_zip_codes, both_zip_codes["Zip Code"] == joined1["ZIPcode"], "inner")
-
+print("Highest 3")
+joined2 = joined1.join(highest_3_zip_codes, highest_3_zip_codes["Zip Code"] == joined1["ZIPcode"], "inner")
 joined2.groupBy("Vict Descent").count().select("Vict Descent", "count").orderBy(col("count").desc()).show() 
 
+print()
+
+print("Lowest 3")
+joined3 = joined1.join(lowest_3_zip_codes, lowest_3_zip_codes["Zip Code"] == joined1["ZIPcode"], "inner")
+joined3.groupBy("Vict Descent").count().select("Vict Descent", "count").orderBy(col("count").desc()).show() 
 
